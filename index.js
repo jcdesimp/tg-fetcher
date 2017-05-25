a46057cfd1554b69a258c4f2546d2f084bf880dc
Joseph DeSimpliciis <joe@jcdesimp.com>
2017-05-25 14:51:52 -0400

    App now connects to correct data center.

--- index.js
+++ index.js
@@ -1,6 +1,91 @@
 const telegramLink = require('telegram.link')();
+const readline = require('readline');
+const CONF = require('./config.json');
 
-telegramLink.on('connect', (data) => {
-  console.log("Connected!");
-  console.log(data);
-})
+const appConf = {
+  id: CONF.appId,
+  hash: CONF.appHash,
+  version: CONF.appVersion,
+  lang: CONF.langCode,
+  deviceModel: 'nodeJS',
+  systemVersion: '6',
+};
+
+let dataCenter = telegramLink.PROD_PRIMARY_DC;
+let tgClient;
+let dataCenters = null;
+let authKey = null;
+let phoneNumber = null;
+
+
+function handleError(err) {
+  console.log(err);
+}
+
+function handleSendAuthCode(result) {
+  if (result.error_code) {
+    if (result.error_code === 303) {
+      // console.log(err);
+      console.log('Changing data center!');
+      const dcRegExp = /.*_([0-9].*)$/g;
+      const targetDC = dcRegExp.exec(result.error_message)[1];
+      dataCenter = dataCenters[`DC_${targetDC}`];
+      tgClient.end();
+      tgClient = telegramLink.createClient(appConf, dataCenter, handleConnect);
+      return;
+    }
+    console.log(result);
+    return;
+  }
+  console.log('CODE SENT!');
+  // console.log(result);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter Auth Code: ', (ans) => {
+    console.log(ans);
+    rl.close();
+  });
+}
+
+function handleGetDataCenters(dcs) {
+  dataCenters = dcs;
+  if (phoneNumber) {
+    tgClient.auth.sendCode(phoneNumber, 5, 'en', handleSendAuthCode);
+  } else {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question('Enter Phone Number: ', (ans) => {
+      phoneNumber = ans;
+      tgClient.auth.sendCode(phoneNumber, 5, 'en', handleSendAuthCode);
+      rl.close();
+    });
+  }
+}
+
+function handleConnect() {
+  console.log('Connected!');
+  // tgClient.getDataCenters(() => {
+
+  // });
+  tgClient.createAuthKey((err, key) => {
+    console.log('Got Key!');
+    authKey = key;
+    // tgClient.getDataCenters((l, c, n) => {
+    //   console.log(l);
+    // });
+    tgClient.getDataCenters(handleGetDataCenters);
+  });
+  // while (!tgClient.isReady()) {
+  //   // do nothing
+  // }
+}
+
+tgClient = telegramLink.createClient(appConf, dataCenter, handleConnect);
+
+// tgClient.on('error', handleError);
+
+process.stdin.resume();
--- scripts/generateConfiguration.js
+++ scripts/generateConfiguration.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 
+const CONFIG_FILE = path.join(__dirname, '../config.json');
+
+const defaultConf = {
+  appId: 'YOUR_APP_ID',
+  appHash: 'APP_HASH',
+  appVersion: '0.0.0',
+};
+
+let newConf = defaultConf;
+
+if (fs.existsSync(CONFIG_FILE)) {
+  const currConf = JSON.parse(fs.readFileSync(CONFIG_FILE));
+  newConf = Object.assign({}, defaultConf, currConf);
+}
+fs.writeFileSync(CONFIG_FILE, JSON.stringify(newConf, null, 2));
